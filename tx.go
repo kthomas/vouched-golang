20fb6ce0203c1e3fac61d7707ed28fef666e68d0
Kyle Thomas <kyle@provide.services>
2019-09-09 14:55:27 -0400

    Return not implemented errors from tx.go

--- tx.go
+++ tx.go
@@ -13,33 +13,15 @@ const VouchedTxTypeTransfer = "transfer"
 
 // EvaluateFraud evaluates a transaction for payment fraud; see https://edoc.vouched.com
 func (v *VouchedAPIClient) EvaluateFraud(params map[string]interface{}) (interface{}, error) {
-	var resp map[string]interface{}
-	status, err := v.Post(fmt.Sprintf("im/transaction?graphScoreResponse=false"), params, &resp)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to evaluate tx for payment fraud via vouched API; status: %d; %s", status, err.Error())
-	}
-	return resp, nil
+	return nil, fmt.Errorf("vouched API method not implemented")
 }
 
 // ReportFraud reports a fraud event; see https://edoc.vouched.com
 func (v *VouchedAPIClient) ReportFraud(params map[string]interface{}) (interface{}, error) {
-	var resp map[string]interface{}
-	status, err := v.Post("im/admin/jax/feg", params, &resp)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to report fraud event via vouched API; status: %d; %s", status, err.Error())
-	}
-	return resp, nil
+	return nil, fmt.Errorf("vouched API method not implemented")
 }
 
 // ReportTransaction reports various kinds of transactions including deposits, withdrawals and internal transfer
 func (v *VouchedAPIClient) ReportTransaction(txType string, params map[string]interface{}) (interface{}, error) {
-	if txType != VouchedTxTypeDeposit && txType != VouchedTxTypeWithdrawal && txType != VouchedTxTypeTransfer {
-		return nil, fmt.Errorf("Invalid tx type provided: %s", txType)
-	}
-	var resp map[string]interface{}
-	status, err := v.Post(fmt.Sprintf("im/account/%s?graphScoreResponse=false", txType), params, &resp)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to report tx via vouched API; status: %d; %s", status, err.Error())
-	}
-	return resp, nil
+	return nil, fmt.Errorf("vouched API method not implemented")
 }
