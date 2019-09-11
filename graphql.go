7e334b0e3a0ea931dfce73fd452dd9ad1010435b
Kyle Thomas <kyle@provide.services>
2019-09-10 20:47:58 -0400

    Fix vouched graphql queries

--- graphql.go
+++ graphql.go
@@ -1,44 +1,111 @@
 package vouched
 
-const graphqlSubmitJobMutation = `mutation {
-	submitJob(input: $input) {
-	  success
-	  type
-	  country
-	  state
-	  confidences {
-		id
-		backId
-		selfie
-		idMatch
-		faceMatch
-	  }
-	}
+const graphqlSubmitJobMutation = `mutation submitJob($type: String!, $callbackURL: String, $params: JobParams) {
+    submitJob(type: $type, callbackURL: $callbackURL, params: $params) {
+      id
+      status
+      request {
+        type
+        callbackURL
+        parameters {
+          idPhoto
+          userPhoto
+          twicPhoto
+          carInsurancePhoto
+          dotPhoto
+          firstName
+          lastName
+          dob
+        }
+      }
+      result {
+        success
+        type
+        country
+        state
+        confidences {
+          id
+          backId
+          selfie
+          idMatch
+          faceMatch
+        }
+      }
+      errors {
+        type
+        message
+        suggestion
+      }
+      submitted
+    }
   }`
 
 const graphqlQueryJobs = `query jobs(
     $id: ID
+    $ids: [ID]
     $type: String
     $status: String
+    $to: String
+    $from: String
     $withPhotos: Boolean
+    $sortOrder: String
+    $sortBy: String
+    $page: Int
+    $pageSize: Int
   ) {
     jobs(
-	  id: $id
-	  type: $type
+      withPhotos: $withPhotos
+      id: $id
+      ids: $ids
       status: $status
-	  withPhotos: $withPhotos
+      type: $type
+      to: $to
+      from: $from
+      sortOrder: $sortOrder
+      sortBy: $sortBy
+      page: $page
+      pageSize: $pageSize
     ) {
       total
       totalPages
       pageSize
       page
       items {
-		id
-		status
-		submitted
-		request
-		result
-		errors
+        id
+        status
+        request {
+          type
+          callbackURL
+          parameters {
+            idPhoto
+            userPhoto
+            twicPhoto
+            carInsurancePhoto
+            dotPhoto
+            firstName
+            lastName
+            dob
+          }
+        }
+        result {
+          success
+          type
+          country
+          state
+          confidences {
+            id
+            backId
+            selfie
+            idMatch
+            faceMatch
+          }
+        }
+        errors {
+          type
+          message
+          suggestion
+        }
+        submitted
       }
     }
   }`
